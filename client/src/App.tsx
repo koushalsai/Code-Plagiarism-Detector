c600843d3e671bc998a15d611146f811ee70a6e3
rishh-e <koona.rajarshi@gmail.com>
2025-06-14 00:13:35 +0530

    Added components and created home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/Home";
+import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <div>
-      <h1>Welcome to the App</h1>
-      <p>This is a simple React application with a Router component.</p>
-    </div>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
 function App() {
   return (
-    <div className="App">
-      <Router />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
 
